0287756af6bc9cc8aade142eba8749735dbd4f61
roshnitikar-dev <ROSHANITALE01@GMAIL.COM>
2025-12-17 23:36:21 +0530

    Added main class to test utility package

---
+++ packages/com/app/main/TestAvni.java
@@ -0,0 +1,9 @@
+package com.app.main;
+import com.app.utility.Avni;
+public class TestAvni {
+public static void main(String[] args) {
+	Avni a1= new Avni();
+	int res=a1.addTwo(5,7);
+	System.out.println("Sum of two numbers = "+res);
+}
+}
