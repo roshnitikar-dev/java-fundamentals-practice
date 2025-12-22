8ab02ea66f57570dda9b7a38becb224de13f694c
roshnitikar-dev <ROSHANITALE01@GMAIL.COM>
2025-12-22 17:18:00 +0530

    Added example of calling multiple methods using same object

---
+++ Objects/com/tka/dec11/NumbersInfo.java
@@ -0,0 +1,12 @@
+package com.tka.dec11;
+
+public class NumbersInfo {
+
+    int addTwo(int a, int b) {
+        return a + b;
+    }
+
+    int multiTwo(int a, int b) {
+        return a * b;
+    }
+}
