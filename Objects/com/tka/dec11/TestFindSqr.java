16d296c317e3a31b73978dfab3b8676215888bf0
roshnitikar-dev <ROSHANITALE01@GMAIL.COM>
2025-12-23 00:26:25 +0530

    Added example of using two objects with same method

---
+++ Objects/com/tka/dec11/TestFindSqr.java
@@ -0,0 +1,12 @@
+package com.tka.dec11;
+
+public class TestFindSqr {
+public static void main(String[] args) {
+	FindSqr f1 = new FindSqr();
+	FindSqr f2 = new FindSqr();
+	int m=f1.sqrNum(7);
+	int n = f2.sqrNum(8);
+	System.out.println("Square of first number is "+m);
+	System.out.println("Square if second number is "+n);
+}
+}
