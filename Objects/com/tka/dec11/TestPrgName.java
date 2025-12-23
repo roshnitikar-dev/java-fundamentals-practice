3c08446ffcfcccd1f7ee3c5b09f1bed6216dbe22
roshnitikar-dev <ROSHANITALE01@GMAIL.COM>
2025-12-23 14:32:38 +0530

    Added example of two objects with different instance variable values

---
+++ Objects/com/tka/dec11/TestPrgName.java
@@ -0,0 +1,16 @@
+package com.tka.dec11;
+
+class TestPrgName {
+
+    public static void main(String[] args) {
+
+        PrgName n1 = new PrgName();
+        PrgName n2 = new PrgName();
+
+        n1.name = "Avni";
+        n1.printName();
+
+        n2.name = "Avinash";
+        n2.printName();
+    }
+}
